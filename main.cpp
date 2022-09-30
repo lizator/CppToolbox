c93e0616d177ec1fb6229b05d462adc78e38c6fc
Alice Awesomest <alice@team.com>
2022-09-30 16:33:23 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+bool bfs() {
+    //TODO
+}
+
+bool dfs() {
+    //TODO
+}
+
+int lcm() {
+    //TODO
+}
+
+int gcd() {
+    //TODO
+}
+
+int Dijkstra() {
+    //TODO
+}
